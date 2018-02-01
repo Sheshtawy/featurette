090588397f9aa4570394dbfe2bc171df9622a0e4
Sheshtawy <hisham.elsheshtawy@gmail.com>
2018-01-31 22:43:40 -0500

    Update featurette to use appfactory and scoped sessions

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,43 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import _app_ctx_stack
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine
+from flask_sqlalchemy import BaseQuery
+from app.db import db
 
-import settings
 
-env = settings.get_env()
+# def create_app(name, config, config_type):
+#     """
+#     App factory
+#     :param name: Application name
+#     :param config: Application config
+#     :param config_type: is the config param object, mapping, pyfile, envvar or json
+#     """
 
-app = Flask(__name__)
-app.config.from_pyfile(env)
+DbSession = db.create_scoped_session()
+# DbSession = scoped_session(sessionmaker(), scopefunc=_app_ctx_stack.__ident_func__)
 
-db = SQLAlchemy(app)
+def create_app(name_handler, config_object):
+    """
+    Application factory
+
+    :param name_handler: name of the application.
+    :param config_object: the configuration object.
+    """
+    app = Flask(name_handler)
+    app.config.from_object(config_object)
+    app.engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+
+    # global DbSession
+    # # BaseQuery class provides some additional methods like
+    # # first_or_404() or get_or_404() -- borrowed from
+    # # mitsuhiko's Flask-SQLAlchemy
+    # DbSession.configure(bind=app.engine, query_cls=BaseQuery)
+    db.init_app(app)
+    @app.teardown_appcontext
+    def teardown(exception=None):
+        global DbSession
+        if DbSession:
+            DbSession.remove()
+
+    return app
