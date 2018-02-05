a0c0140a65aacb08a69da044da23678572e35bca
Sheshtawy <hisham.elsheshtawy@gmail.com>
2018-02-05 01:19:40 -0500

    Update static files to add create feature request form

--- static/index.js
+++ static/index.js
@@ -1,47 +1,123 @@
-function FeatureRequest(title, description, client, clientPriority, productArea, targetDate) {
+function FeatureRequest(data) {
     var self = this;
-    self.title = ko.observable(title);
-    self.description = ko.observable(description);
-    self.client = ko.observable(client);
-    self.clientPriority = ko.observable(clientPriority);
-    self.productArea = ko.observable(productArea);
-    self.targetDate = ko.observable(targetDate);
+    self.title = ko.observable(data.title).extend({
+        isRequired: true,
+    });
+    self.description = ko.observable(data.description);
+    self.client = ko.observable(data.client);
+    self.clientPriority = ko.observable(data.clientPriority);
+    self.productArea = ko.observable(data.productArea);
+    self.targetDate = ko.observable(data.targetDate);
 
-    self.saveToServer = function() {
-        var json = ko.toJSON(self);
-    }
-
-    self.edit = function() {
-        var json = ko.toJSON(self);
-    }
-
-    self.openEditModal= function(data, event) {
+    self.showModal = function() {
         $('#editFeatureRequest').modal()
     }
 
-}
+};
+
+function Client(data) {
+    var self = this;
+    self.name = data.name;
+    self.id = data.id;
+};
 
 function MainViewModel() {
     var self = this;
-    self.featureRequests = ko.observableArray([
-        new FeatureRequest('A new very innovative feature request', 
-            'This feature request is going to change the app completely into something awesome!', 
-            { id: 1, name: 'Client A' }, 
-            1,
-            'Policies',
-            Date().toLocaleString()
-        ),
-        new FeatureRequest('Another new very innovative feature request', 
-            'This feature request is going to change the app completely into something awesome!', 
-            { id: 1, name: 'Client B' }, 
-            1,
-            'Policies',
-            Date().toLocaleString()
-        ),
+    self.productAreaEnum = ko.observableArray([
+         'Policies',
+         'Billing',
+         'Claims',
+         'Reports',
     ]);
-    self.addFeatureRequest = function() {
 
+    self.clients = ko.observableArray();
+
+    self.featureRequests = ko.observableArray([
+        new FeatureRequest({
+            title: 'A new very innovative feature request', 
+            description: 'This feature request is going to change the app completely into something awesome!', 
+            client:{ id: 1, name: 'Client A' }, 
+            clientPriority: 1,
+            productArea: 'Policies',
+            targetDate: Date().toLocaleString()
+        }),
+        new FeatureRequest({
+            title: 'Another very innovative feature request', 
+            description: 'This super awesome feature request is going to change the app completely into something awesome!', 
+            client:{ id: 2, name: 'Client B' }, 
+            clientPriority: 2,
+            productArea: 'Policies',
+            targetDate: Date().toLocaleString()
+        }),
+    ]);
+
+    self.modalData = ko.observable(new FeatureRequest(
+        {
+            title: 'A new very innovative feature request', 
+            description: 'This feature request is going to change the app completely into something awesome!', 
+            client:{ id: 1, name: 'Client A' }, 
+            clientPriority: 1,
+            productArea: 'Policies',
+            targetDate: Date().toLocaleString()
+        }
+    ));
+
+    self.setModalData = function(featureRequest) {
+        console.log(featureRequest)
+        self.modalData(new FeatureRequest(ko.toJS(featureRequest)));
+        console.log(self.modalData);
+        featureRequest.showModal();
+    };
+
+    self.ToggleAddFeature = function() {
+        console.log('toogggllee')
+        $("#addFeatureRequest").modal();
     }
+
+    self.addFeatureRequest = function() {
+        var dataObject = {};
+        var data = $('#addFeatureRquestForm').serializeArray();
+        console.log($('#addFeatureRquestForm'))
+        data.forEach(function(item) {
+            dataObject[item.name] = item.value;
+        });
+        console.log(dataObject, data)
+        $.ajax(
+            '/feature_requests/',
+            {
+                contentType: 'application/json;',
+                method: 'POST',
+                data: JSON.stringify(dataObject),
+                success: function (response) {
+                    $('#addFeatureRequest').modal('hide');
+                    self.featureRequests.push(new FeatureRequest(ko.toJS(response)));
+                },
+            }
+        );
+
+        $('#addFeatureRequest').modal('hide');
+    }
+
+    self.updateFeatureRequest = function() {}
 }
 
-ko.applyBindings(new MainViewModel())
+// extenders 
+// validations
+
+var isRequired = function(target, isRequired) {
+    
+    var validate = function(value) {
+        if(isRequired || (!value || value === undefined || value === '')) {
+            value.errors ? value.errors['isRequired'] = 'required' : value.errors = { required: 'required' }
+        }
+    }
+
+    validate(target);
+    target.subscribe(validate);
+
+    return target;
+}
+
+ko.extenders.isRequired = isRequired;
+
+ko.applyBindings(new MainViewModel(), document.getElementById('#root'));
